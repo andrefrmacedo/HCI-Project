c67ae59c9ca553b2f90fe0e4699785dec15fd7ff
André Macedo <andrefrmacedo@gmail.com>
2015-11-18 02:01:56 +0000

    Deleted commented code

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -4,10 +4,4 @@ export default Ember.Route.extend({
 	model: function(){
 		return this.store.query('video',{orderBy: 'userID', equalsTo: this.get('session.uid')});
 	},
-
-	/*afterModel: function(videos, transition){
-		videos.map(item=>{
-			console.log(item.get('response.items'));
-		});
-	}*/
 });
