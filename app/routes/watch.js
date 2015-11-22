76e5f7b27538b9b85d0375820dac27a475c9ce97
André Macedo <andrefrmacedo@gmail.com>
2015-11-22 04:07:28 +0000

    Changed some features in ember-youtube addon

--- app/routes/watch.js
+++ app/routes/watch.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
 	// model: function(){
 	// 	return this.store.find('video',video);
 	// }
+	actions: {
+		test: function(myPlayer){
+			myPlayer.send('refreshCurrentTime');
+			console.log(myPlayer.get('currentTime'));
+		}
+	}
 });
