5a0701f6e25e19dd0fb2d60cc89156840a725bd7
André Macedo <andrefrmacedo@gmail.com>
2015-11-26 12:09:51 +0000

    Added notes model

--- app/models/video.js
+++ app/models/video.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	userID: DS.attr('string'),
 	youtube: DS.attr(),
-	
+	notes: DS.hasMany('note'),
 });
--- app/routes/watch.js
+++ app/routes/watch.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	// model: function(){
-	// 	return this.store.find('video',video);
-	// }
+	
 	actions: {
 		test: function(myPlayer){
 			myPlayer.send('refreshCurrentTime');
