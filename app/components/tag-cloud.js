705eb8f37ee31eb8a5d79f8a22cbd1db10d4ec03
André Macedo <andrefrmacedo@gmail.com>
2015-12-01 19:55:30 +0000

    Tag-cloud works with array of keywords

--- app/components/tag-cloud.js
+++ app/components/tag-cloud.js
@@ -1,37 +1,63 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	setup: function(){
-	var fill = d3.scale.category20();
+	
+	words: function(){
+		let arr = this.get('model.notes');
+			let count = { };
+			let wordCount = [];
 
-  	d3.layout.cloud().size([300, 300])
-      	.words([
-        	".NET", "Silverlight", "jQuery", "CSS3", "HTML5", "JavaScript", "SQL","C#"].map(function(d) {
-        	return {text: d, size: 10 + Math.random() * 50};
-      	}))
-      	.rotate(function() { return ~~(Math.random() * 2) * 90; })
-      	.font("Impact")
-      	.fontSize(function(d) { return d.size; })
-      	.on("end", draw)
-      	.start();
+			this.get('model.notes').map(note => {
+				
+				if(count[note.get('keyword')]){
+					count[note.get('keyword')]++;
+				}
+				else{
+					count[note.get('keyword')]=1;
+				}
+			});
 
-  	function draw(words) {
-    	d3.select("body").append("svg")
-        	.attr("width", 300)
-        	.attr("height", 300)
-      	.append("g")
-        	.attr("transform", "translate(150,150)")
-      	.selectAll("text")
-        	.data(words)
-      	.enter().append("text")
-        	.style("font-size", function(d) { return d.size + "px"; })
-        	.style("font-family", "Impact")
-        	.style("fill", function(d, i) { return fill(i); })
-        	.attr("text-anchor", "middle")
-        	.attr("transform", function(d) {
-        	  return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-        	})
-        	.text(function(d) { return d.text; });
-  		}
-	}.on('init')
+			for(var key in count){
+				wordCount.push({
+					text: key,
+					size: 100*count[key]/this.get('model.notes.length')+15,
+				});
+			}
+			
+		return wordCount;
+	},
+
+	actions: {
+		showCloud: function(){
+			
+			let fill = d3.scale.category20();
+
+		  	d3.layout.cloud().size([300, 300])
+		      	.words(this.words())
+		      	.rotate(function() { return ~~(Math.random() * 2) * 90; })
+		      	.font("Impact")
+		      	.fontSize(function(d) { return d.size; })
+		      	.on("end", draw)
+		      	.start();
+
+		  	function draw(words) {
+		    	d3.select("body").append("svg")
+		        	.attr("width", 300)
+		        	.attr("height", 300)
+		      	.append("g")
+		        	.attr("transform", "translate(150,150)")
+		      	.selectAll("text")
+		        	.data(words)
+		      	.enter().append("text")
+		        	.style("font-size", function(d) { return d.size + "px"; })
+		        	.style("font-family", "Impact")
+		        	.style("fill", function(d, i) { return fill(i); })
+		        	.attr("text-anchor", "middle")
+		        	.attr("transform", function(d) {
+		        	  return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+		        	})
+		        .text(function(d) { return d.text; });
+		  	}
+		}
+	}
 });
